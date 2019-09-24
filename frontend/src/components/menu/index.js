786f7b98d1f92e99bdf333c5844353f6f2ee90e6
Unknown <opticalfalicy@gmail.com>
2019-09-24 07:12:02 -0400

    Styling

--- frontend/src/components/menu/index.js
+++ frontend/src/components/menu/index.js
@@ -6,9 +6,35 @@ class Menu extends Component {
   render () {
     return (
       <div className="menu-page main-item">
-        <div className="menu">
-          <div className="menu-text" />
-        </div>
+        <table className="menu">
+          <thead>
+            <tr>
+              <th colSpan="2">Platter</th>
+            </tr>
+          </thead>
+          <tbody className="menu-text">
+            <tr>
+              <td>Lamb Over Rice</td>
+              <td>$6.00</td>
+            </tr>
+            <tr>
+              <td>Falafel Over Rice</td>
+              <td>$6.00</td>
+            </tr>
+            <tr>
+              <td>Chicken Over Rice</td>
+              <td>$6.00</td>
+            </tr>
+            <tr>
+              <td>Fish Over Rice</td>
+              <td>$6.00</td>
+            </tr>
+            <tr>
+              <td>Kebab Over Rice</td>
+              <td>$6.00</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
